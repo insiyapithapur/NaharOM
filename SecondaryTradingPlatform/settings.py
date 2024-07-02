6b3d3378a83099c646fa5e2819d2734b77abe99b
insiyapithapur <banupithapur@gmail.com>
2024-07-02 10:49:35 +0530

    commit

--- SecondaryTradingPlatform/settings.py
+++ SecondaryTradingPlatform/settings.py
@@ -134,5 +134,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = os.path.join(BASE_DIR,'static') ,
-STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles_build','static')
+# STATICFILES_DIRS = os.path.join(BASE_DIR,'static') ,
+# STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles_build','static')
+
+STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
