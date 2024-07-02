26c514cc7d3f407bdeec9725051aed91cde5dba2
insiyapithapur <banupithapur@gmail.com>
2024-07-02 10:45:51 +0530

    commit

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,6 @@
 echo " BUILD START "
 python3.12 -m pip install -r requirements.txt
+python3.12 manage.py makemigrations --noinput
+python3.12 manage.py migrate --noinput
 python3.12 manage.py collectstatic --noinput --clear
 echo " BUILD END "
