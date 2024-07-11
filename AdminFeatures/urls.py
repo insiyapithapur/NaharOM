4282fe6e33c1a9efd39eafd9ba8d925957d9babc
insiyapithapur <banupithapur@gmail.com>
2024-07-11 11:00:18 +0530

    commit

--- AdminFeatures/urls.py
+++ AdminFeatures/urls.py
@@ -2,7 +2,7 @@ from django.urls import path , include
 from . import views
 
 urlpatterns = [
-    path('AdminLogin/',views.AdminLoginAPI),
+    path('Login/',views.LoginAPI),
     path('ExtractInvoicesAPI/',views.ExtractInvoicesAPI),
     # used to send all invoices from primary to secondary platform
     path('GetInvoices/<int:user_id>/', views.GetInvoicesAPI), # primary to secondary all invoices show
