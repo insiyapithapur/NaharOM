1781521045d087e7570227639ca266a6980c7595
insiyapithapur <banupithapur@gmail.com>
2024-09-11 20:21:52 +0530

    update

--- UserFeatures/views.py
+++ UserFeatures/views.py
@@ -1054,19 +1054,19 @@ def TobuyAPI(request):
                     unit.unitID.save()
 
                     print("ksjcbd b")
-                    salespurchaseReport = models.SalePurchaseReport.objects.create(
-                        invoiceID = postForSale.invoice_id ,
-                        unitID = unit.unitID ,
-                        seller_ID = postForSale.user_id,
-                        buyerID_ID = user_role ,
-                        Sale_Buy_Date = timezone.now().date(),
-                        Sale_Buy_per_unit_price = postForSale.per_unit_price,
-                        ListingDate = timezone.now().date(),
-                        no_of_days_units_held = (sales.sell_date - postForSale.post_date).days,
-                        interest_due_to_seller = ( postForSale.per_unit_price * 10 ) / 100,
-                        TDS_deducted = ( postForSale.per_unit_price * 10 ) / 100,
-                        IRR = postForSale.invoice_id.irr
-                    )
+                    # salespurchaseReport = models.SalePurchaseReport.objects.create(
+                    #     invoiceID = postForSale.invoice_id ,
+                    #     unitID = unit.unitID ,
+                    #     seller_ID = postForSale.user_id,
+                    #     buyerID_ID = user_role ,
+                    #     Sale_Buy_Date = timezone.now().date(),
+                    #     Sale_Buy_per_unit_price = postForSale.per_unit_price,
+                    #     ListingDate = timezone.now().date(),
+                    #     no_of_days_units_held = (sales.sell_date - postForSale.post_date).days,
+                    #     interest_due_to_seller = ( postForSale.per_unit_price * 10 ) / 100,
+                    #     TDS_deducted = ( postForSale.per_unit_price * 10 ) / 100,
+                    #     IRR = postForSale.invoice_id.irr
+                    # )
                 print("dbjcksb")
                 
                 buyer_wallet.OutstandingBalance -= total_price
